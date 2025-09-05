f1f8dea021e77aaab79f587f847e55d58e064cf7
C4 <147143261+C-4-C-4@users.noreply.github.com>
2025-09-05 12:04:21 +0800

    底部版权信息更改

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -139,8 +139,7 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/C-4-C-4/xcloudx-wiki' }
     ],
     footer: {
-      message: '<a href="https://icp.govs.xin/id.php?keyword=20252025" target="_blank">信ICP备20252025号</a>',
-      copyright: '<a href="https://www.govs.cc/id.php?keyword=20252025" target="_blank">嘣ICP备20252025号</a>',
+      copyright: 'Copyright@ 2025 Albert XcoudX',
     }
   }
 })
